bc9da2fbf9d9293f328aba5d5aa1176c82166514
li6in9muyou <libq5520@mails.jlu.edu.cn>
2024-11-18 20:34:28 +0800

    ok: vanilla html

---
+++ main.js
@@ -0,0 +1,16 @@
+function render(props, state) {
+  console.log("libq render", props, state);
+  $("main").html("");
+  console.log("libq main is cleared");
+
+  for (let i = 0; i < props.cnt; i++) {
+    const oprand = i + 1;
+    const row = $("<p>");
+    row.text(`${oprand} x ${oprand} = ${oprand * oprand}`);
+    console.log("libq row", row[0], row);
+    $("main").append(row);
+    console.log("libq inserted", oprand);
+  }
+}
+
+render({ cnt: 10 });
