bf0d016a28b9fbe94cea4b3967d90998494f99ab
li6in9muyou <libq5520@mails.jlu.edu.cn>
2024-12-22 14:46:41 +0800

    chore: remove debug code

--- src/main.js
+++ src/main.js
@@ -4,15 +4,12 @@ import { multiply } from "./lib/calc.js";
 function render(props, state) {
   console.log("libq render", props, state);
   $("main").html("");
-  console.log("libq main is cleared");
 
   for (let i = 0; i < props.cnt; i++) {
     const oprand = i + 1;
     const row = $("<p>");
     row.text(`${oprand} x ${oprand} = ${multiply(oprand, oprand)}`);
-    console.log("libq row", row[0], row);
     $("main").append(row);
-    console.log("libq inserted", oprand);
   }
 }
 
