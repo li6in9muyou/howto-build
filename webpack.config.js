cdeea5c0099c12a210d1463441e663ff7b5bb6cd
li6in9muyou <libq5520@mails.jlu.edu.cn>
2024-12-22 15:32:30 +0800

    ok: add source maps and file hashes

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,10 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
+  devtool: "source-map",
   entry: "./src/main.js",
   output: {
-    filename: "bundle.js",
+    filename: "[name].[hash:6].js",
     path: path.join(__dirname, "dist"),
     clean: true,
   },
